eced71939635a17e57f23bcb3665e028847df16a
alvin <alvin@DESKTOP-CH61C17>
2017-07-31 04:14:32 -0400

    Created game.js

---
+++ game.js
@@ -0,0 +1,20 @@
+﻿var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+
+// Background image 
+
+var backgroundReady = false;
+var backgroundImg = new Image();
+backgroundImg.onload = function () {
+    backgroundReady = true;
+}
+backgroundImg.src = "images/background.jpg";
+
+
+// DISPLAY OBJECTS 
+
+function render() {
+    if (backgroundReady) {
+        ctx.drawImage(backgroundImg, 0, 0);
+    }
+}
